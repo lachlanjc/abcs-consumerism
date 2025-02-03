8f1b2ed5419cdebebd28867f07eb1c455d6a5260
Lachlan Campbell <lachlanjc@hey.com>
2025-02-03 05:10:12 -0500

    Fix layout import

--- src/word-processing.ts
+++ src/word-processing.ts
@@ -1,7 +1,6 @@
 export default function wordProcessing() {
   // All remark and rehype plugins return a separate function
   return function (tree, file) {
-    file.data.astro.frontmatter.layout =
-      file.data.astro.frontmatter.layout || "./src/layouts/Word.astro"
+    file.data.astro.frontmatter.layout ??= "../layouts/Word.astro"
   }
 }
