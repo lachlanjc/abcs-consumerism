36551a6133e35bb3f112291425f069357b548f03
Lachlan Campbell <lachlanjc@hey.com>
2025-02-06 03:38:22 -0500

    Add font fallbacks

--- src/word-processing.ts
+++ src/word-processing.ts
@@ -1,6 +1,7 @@
 export default function wordProcessing() {
   // All remark and rehype plugins return a separate function
-  return function (tree, file) {
+  return function (_: unknown, file: unknown) {
+    // @ts-expect-error haven't typed the file object
     file.data.astro.frontmatter.layout ??= "../layouts/Word.astro"
   }
 }
