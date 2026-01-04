ebc99d0ae3ec9fd1d2f67b440be0c72b178b1c08
Lachlan Campbell <lachlan@watershedclimate.com>
2026-01-04 02:00:42 -0800

    Use Astro Fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,30 @@ import remarkWikiLink from "remark-wiki-link"
 
 // https://astro.build/config
 export default defineConfig({
+  experimental: {
+    fonts: [
+      {
+        provider: "local",
+        name: "ABCDailySlab",
+        cssVariable: "--font-serif",
+        variants: [
+          {
+            src: ["./src/fonts/ABCDailySlabVariableEdu-Light.woff2"],
+          },
+        ],
+      },
+      {
+        provider: "local",
+        name: "ABCDailySlabMono",
+        cssVariable: "--font-mono",
+        variants: [
+          {
+            src: ["./src/fonts/ABCDailySlabMonoEdu-Light.woff2"],
+          },
+        ],
+      },
+    ],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
